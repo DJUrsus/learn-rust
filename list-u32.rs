44d4f0fc395e00c6a6beb19a87e1236a6c07875c
Colin von Heuring <colinvh@divitu.com>
2014-07-04 12:33:28 -0700

    cddar

---
+++ list-u32.rs
@@ -0,0 +1,41 @@
+fn main() {
+
+	#[deriving(Clone)]
+	enum List {
+		Cons(u32, Box<List>),
+		Nil,
+	}
+
+	fn prepend(xs: List, value: u32) -> List {
+		Cons(value, box xs)
+	}
+
+	fn eq(xs: &List, ys: &List) -> bool {
+		match (xs, ys) {
+			(&Nil, &Nil)
+				=> true,
+
+			(&Cons(x, box ref next_xs), &Cons(y, box ref next_ys))
+			if x == y
+				=> eq(next_xs, next_ys),
+
+			_   => false,
+		}
+	}
+
+	let xs = Cons(5, box Cons(10, box Nil));
+	let ys = Cons(5, box Cons(10, box Nil));
+	let mut zs = ys.clone();
+
+	assert!(eq(&xs, &ys));
+	assert!(eq(&ys, &zs));
+
+	zs = prepend(zs, 0);
+	assert!(!eq(&ys, &zs));
+	match zs {
+		Cons(_, box ref next) =>
+			assert!(eq(&ys, next)),
+		_ => (),
+	}
+
+}
---
+++ list.rs
@@ -0,0 +1,41 @@
+fn main() {
+
+	#[deriving(Clone)]
+	enum List<T> {
+		Cons(T, Box<List<T>>),
+		Nil,
+	}
+
+	fn prepend<T>(xs: List<T>, value: T) -> List<T> {
+		Cons(value, box xs)
+	}
+
+	fn eq<T: PartialEq>(xs: &List<T>, ys: &List<T>) -> bool {
+		match (xs, ys) {
+			(&Nil, &Nil)
+				=> true,
+
+			(&Cons(ref x, box ref next_xs), &Cons(ref y, box ref next_ys))
+			if x == y
+				=> eq(next_xs, next_ys),
+
+			_   => false,
+		}
+	}
+
+	let xs = Cons('c', box Cons('a', box Cons('t', box Nil)));
+	let ys = Cons('c', box Cons('a', box Cons('t', box Nil)));
+	let mut zs = ys.clone();
+
+	assert!(eq(&xs, &ys));
+	assert!(eq(&ys, &zs));
+
+	zs = prepend(zs, 's');
+	assert!(!eq(&ys, &zs));
+	match zs {
+		Cons(_, box ref next) =>
+			assert!(eq(&ys, next)),
+		_ => (),
+	}
+
+}
