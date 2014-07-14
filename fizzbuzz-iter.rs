b92adf59ec4a6938e01f2534b6e246ef626c69d2
Colin von Heuring <colinvh@divitu.com>
2014-07-13 18:28:21 -0700

    idiomatic constructor

--- fizzbuzz-iter.rs
+++ fizzbuzz-iter.rs
@@ -2,6 +2,10 @@ struct FizzBuzzer {
 	num: uint
 }
 
+impl FizzBuzzer {
+	fn new() -> FizzBuzzer { FizzBuzzer { num: 0 } }
+}
+
 impl Iterator<String> for FizzBuzzer {
 	fn next(&mut self) -> Option<String> {
 		self.num += 1;
@@ -14,11 +18,8 @@ impl Iterator<String> for FizzBuzzer {
 	}
 }
 
-// I don't like this name, and neither does Rust.
-fn new_FizzBuzzer() -> FizzBuzzer { FizzBuzzer { num: 0 } }
-
 fn main() {
-	let mut i = new_FizzBuzzer();
+	let mut i = FizzBuzzer::new();
 	for fb in i {
 		print!("{}\n", fb);
 	}
